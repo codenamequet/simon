4499c36631585d4fe13216b39c7cf2642ef0fd1a
Your Name <you@example.com>
2017-12-11 11:13:21 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// create 4 "buttons" of some kind
+// color code those buttons
+// make the buttons light up
+// make the lights go in a random pattern that repeats with an extra light every turn after a user repeats the pattern by pressing the button
+// when a user makes a mistake alert them that they lost and give them the option to try again
