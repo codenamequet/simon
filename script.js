2a940f6449de946dc95cf62ae588da10902bfa5b
Your Name <you@example.com>
2017-12-12 10:28:35 -0500

    started adding algorithm to have lights glow on their own

--- script.js
+++ script.js
@@ -5,20 +5,13 @@
 // when a user makes a mistake alert them that they lost and give them the option to try again
 
 
-//make colors glow when clicked on
-// function glowUp() {
-//     var grabColor = document.getElementsByClassName('main-buttons')[0]
-//     grabColor.addEventListener('click', function() {
-//         grabColor.style.background = '#4682B4'
-//          })
-// }
-
 var blueButton = document.getElementsByClassName('main-buttons')[0]
 var redButton = document.getElementsByClassName('main-buttons')[1]
 var greenButton = document.getElementsByClassName('main-buttons')[3]
 var yellowButton = document.getElementsByClassName('main-buttons')[2]
 
-function glowUp() {
+//make colors glow when clicked on
+function userInput() {
     for (i = 0; i < 4; i++) {
     var grabColor = document.getElementsByClassName('main-buttons')[i]
     grabColor.addEventListener('click', function() {
@@ -31,6 +24,29 @@ function glowUp() {
     }
 }
 
-glowUp()
+userInput()
+
+// var random = Math.floor((Math.random() * 4) + 1)
+var lightSequence = []
+
+function glowUp () {
+    for (i = 0; i < 100; i++) {
+        var  randomColor = document.getElementsByClassName('main-buttons')[i]
+        lightSequence = this.random //select a random button
+        // setTimeout(function() {blueButton.style.opacity = '1'}, 500)
+        // setTimeout(function() {redButton.style.opacity = '1'}, 500)
+        // setTimeout(function() {greenButton.style.opacity = '1'}, 500)
+        // setTimeout(function() {yellowButton.style.opacity = '1'}, 500)
+        if (userInput !== lightSequence) {
+            alert('Wrong button! Game Over.')
+        }
+    }
+}
+
+// while loop to loop through a randomize function to make items light up by their self.
+// store every item that gets looped through in an empty array
+// await user input before adding more items
+// if user input !== value in array end loop
 
 // opacity https://www.w3schools.com/jsref/prop_style_opacity.asp
+// random numbers javascript & jquery book
