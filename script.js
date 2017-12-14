e5dedd721fbbabbba8fbd8ad79f6cb99e4f2f496
Your Name <you@example.com>
2017-12-14 14:05:13 -0500

    getting ready to deploy

--- script.js
+++ script.js
@@ -12,24 +12,41 @@ var game = {
 }
 
 // turns lights off after set amount of time
-function lightsOff () {
-    setTimeout(function() {blueButton.style.opacity = '1'}, 500)
-    setTimeout(function() {redButton.style.opacity = '1'}, 500)
-    setTimeout(function() {greenButton.style.opacity = '1'}, 500)
-    setTimeout(function() {yellowButton.style.opacity = '1'}, 500)
+function lightsOff (index) {
+    if (!index) {
+        setTimeout(function() {blueButton.style.opacity = '1'}, 500)
+        setTimeout(function() {redButton.style.opacity = '1'}, 500)
+        setTimeout(function() {greenButton.style.opacity = '1'}, 500)
+        setTimeout(function() {yellowButton.style.opacity = '1'}, 500)
+    } else {
+        setTimeout(function() {blueButton.style.opacity = '1'}, (500 * index))
+        setTimeout(function() {redButton.style.opacity = '1'}, (500 * index))
+        setTimeout(function() {greenButton.style.opacity = '1'}, (500 * index))
+        setTimeout(function() {yellowButton.style.opacity = '1'}, (500 * index))
+    }
 }
+
+// function lightsOff (light) {
+//     for (var i = 0; i < game.lightSequence.length; i++) {
+//         setTimeout(function() {light.style.opacity = '1'}, (500 * light.index))
+//     }
+// }
+
+// var lightsOff2 = lightsOff(i)
 
 //lets simon select random lights
 function glowUp () {
     playLastSequence()
+    lightsOff()
     let randomLight = (Math.floor(Math.random() * game.lights.length))
     var lightUp = document.getElementsByClassName('main-buttons')[randomLight]
     lightUp.style.opacity = '0.5'
-    lightsOff()
+    // lightsOff()
     game.lightSequence.push(lightUp.classList[0])
     console.log(game.lightSequence)
     // handleUserChoice()
 }
+
 function handleUserChoice () { 
         console.log('this is round ' + game.round)
         if (JSON.stringify(game.lightSequence) === JSON.stringify(game.userSequence)) {
@@ -78,66 +95,44 @@ clickListener()
 
 // return the last lightSequence 
 function playLastSequence() {
+    // lightsOff(game.lightSequence)
     for (i = 0; i < game.lightSequence.length; i++) {
         var reLight = game.lightSequence[i]
         console.log(game.lightSequence[i])
         var colors = {
              blue: blueButton,
-             red:redButton,
+             red: redButton,
              green: greenButton,
              yellow: yellowButton
         }
-        colors[game.lightSequence[i]].style.opacity = '0.5'
-        // colors.style.opacity = '0.5'
-        // let blue = document.getElementsByClassName('main-button')[0]
-        // let red = document.getElementsByClassName('main-button')[1]
-        // let green = document.getElementsByClassName('main-button')[2]
-        // let yellow = document.getElementsByClassName('main-button')[3]
-
-        
-        
-        
-        // this.style.opacity = '0.5'
-        // console.log(reLight)
+        colors[reLight].style.opacity = '0.5'
+        lightTimeOut(i)
+        //stagger time out between inexes of array by a second
     }
 }
+var timerStart = Date.now()
+function lightTimeOut (i) {
+    setTimeout(function () {
+        console.log(game.lightSequence[i])
+        console.log(`time of logging is ${Date.now()-timerStart} and i is ${i}`)
+    }, 1000 + (3000 * i))
+}
+// function turnOff () {
+//     for (i = 0; i < game.lightSequence.length; i++) {
+        ( function (num) 
+        {
+            setTimeout(function () { console.log(num);}, 1000 + (3000 * num));
+        }) (i);
+//     }
+// }
+// var colors = {
+//     blue: blueButton,
+//     red: redButton,
+//     green: greenButton,
+//     yellow: yellowButton
+// }
 // for loop through lightSequence indexes target dom elements
 //add a handleUserChoice function here
-
-// If lightSequence = userSequence move on to the next round and run glow up again
-// If lightSequence != userSequence give failure message
-// after returning true clear both arrays before staring glowUp again
-// make glowUp re-run it's array and then add 1 more to it
-// function handleUserChoice () { 
-//     for (i = 0; i < game.round; i++) {
-//         console.log('this is round ' + game.round)
-//         if (game.round === 1) {
-//             return 
-//         } else if (JSON.stringify(game.lightSequence) === JSON.stringify(game.userSequence)) {
-//         game.userSequence.length = // clear userSequence array
-//         console.log(game.userSequence)
-//         // game.round ++
-//         // console.log(game.round)
-//         //glowUp()
-//         //clear userSequence array, return lightSequence array and start glowUp again
-//         // stringify and compare whole array
-//         } else {
-//         console.log('You failed!')
-//         }
-//     }
-// }
-
-// function handleUserChoice () {
-//     if (game.lightSequence[i] === game.userSequence[i]) {
-//         game.round ++
-//         console.log(game.round++)
-//         userSequence = []
-//         console.log(userSequence)
-//         //glowUp()
-//     } else {
-//         console.log('You failed!')
-//     }
-// }
 
 // opacity https://www.w3schools.com/jsref/prop_style_opacity.asp
 // random numbers javascript & jquery book
