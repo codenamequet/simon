a0a997cb6a153886f2ec92516309187c7b275224
Marquet Reid <you@example.com>
2018-03-11 12:36:25 -0400

    changed all variables to let/const and changed functions to arrow functions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var blueButton = document.getElementsByClassName('main-buttons')[3]
-var redButton = document.getElementsByClassName('main-buttons')[2]
-var greenButton = document.getElementsByClassName('main-buttons')[1]
-var yellowButton = document.getElementsByClassName('main-buttons')[0]
+const blueButton = document.getElementsByClassName('main-buttons')[3]
+const redButton = document.getElementsByClassName('main-buttons')[2]
+const greenButton = document.getElementsByClassName('main-buttons')[1]
+const yellowButton = document.getElementsByClassName('main-buttons')[0]
        
 //object for running the game and rounds
-var game = {
+const game = {
     round: 0,
     lights: ['blue', 'red', 'green', 'yellow'],
     lightSequence: [],
@@ -12,91 +12,87 @@ var game = {
 }
 
 // turns lights off after set amount of time
-function lightsOff () {
-    setTimeout(function() {blueButton.style.opacity = '1'}, 500)
-    setTimeout(function() {redButton.style.opacity = '1'}, 500)
-    setTimeout(function() {greenButton.style.opacity = '1'}, 500)
-    setTimeout(function() {yellowButton.style.opacity = '1'}, 500)
+const lightsOff = () => {
+    setTimeout(() => {blueButton.style.opacity = '1'}, 500)
+    setTimeout(() => {redButton.style.opacity = '1'}, 500)
+    setTimeout(() => {greenButton.style.opacity = '1'}, 500)
+    setTimeout(() => {yellowButton.style.opacity = '1'}, 500)
 }
 
 //lets simon select random lights
-function glowUp () {
-    // playLastSequence()
+const glowUp = () => {
     let randomLight = (Math.floor(Math.random() * game.lights.length))
-    var lightUp = document.getElementsByClassName('main-buttons')[randomLight]
-    // lightUp.style.opacity = '0.5'
+    let lightUp = document.getElementsByClassName('main-buttons')[randomLight]
     lightsOff()
     game.lightSequence.push(lightUp.classList[0])
     playLastSequence()
     console.log(game.lightSequence)
-    // handleUserChoice() // not running this b/c lightSequence != empty beggining userSequence 
 }
 
-//not running if game.round === 0 w/ glowUp() b/c it runs twice and then fails
-function handleUserChoice () { 
-        console.log('this is round ' + game.round)
-        if (JSON.stringify(game.lightSequence) === JSON.stringify(game.userSequence)) {
+const handleUserChoice = () => {
+    if (JSON.stringify(game.lightSequence) === JSON.stringify(game.userSequence)) {
         game.userSequence = []// clear userSequence array
         console.log(game.userSequence)
-        // glowUp()
-        //run glowUp or light up lights agai
-        game.round ++
+        game.round += 100
         console.log(game.round)
         glowUp()
-        //clear userSequence array, return lightSequence array and start glowUp again
-        // stringify and compare whole array
         } else {
         alert('Wrong button! Game Over!')
         location.reload()
     }
 }
-    
-//button runs glowUp...want it to run initial game glow
-function startButton () {
+
+//working on a funtion that gives user a set amount of time before ending game
+
+let delay = () => {
+    for (i = 0; i < 100; i ++) {
+        setTimeout(() => {
+            clickListener
+        }, (i + 3) * 1000)
+    }
+}
+// delay ()
+
+//button runs glowUp to start the game
+const startButton = () => {
     let start = document.getElementsByClassName('start-button')[0]
     start.addEventListener('click', () => {
         glowUp()
     })
-    /// re write start.addEventListener('click', glowUp)
 }
 
 startButton()
 
 //button simply runs handleUserChoice for now
-function compareButton () {
+const endTurn = () => {
     let compare = document.getElementsByClassName('compare-button')[0]
     compare.addEventListener('click', () => {
         handleUserChoice()
     })
 }
 
-compareButton()
+endTurn()
 
 //make lights glow when clicked on
-function clickListener() {
+const clickListener = () => {
     for (i = 0; i < 4; i++) {
-     let grabColor = document.getElementsByClassName('main-buttons')[i]
-     grabColor.addEventListener('click', function() {
-         this.style.opacity = '0.5'
-         console.log('userInput', this, this.classList[0])
-         game.userSequence.push(this.classList[0])//outputs color instead of class name
-            // game.userSequence.push(this) // push the choice into userchoices array
-            // increment the round counter
-            //turns glowing lights back to normal color
-            lightsOff()
-            scoreBoard()
-        })
-    }
+        let grabColor = document.getElementsByClassName('main-buttons')[i]
+        grabColor.addEventListener('click', function() {
+            this.style.opacity = '0.5'
+            console.log('userInput', this, this.classList[0])
+            game.userSequence.push(this.classList[0])//outputs color instead of class name
+               lightsOff()
+               scoreBoard()
+           })
+       }
 }
 
 clickListener()
 
 //return the last lightSequence 
-function playLastSequence() {
+const playLastSequence = () => {
     for (let i = 0; i < game.lightSequence.length; i++) {
-        // var reLight = game.lightSequence[i]
-        // console.log('this is reLight' + game.lightSequence[i])
-        var colors = {
+        const colors = {
              blue: blueButton,
              red:redButton,
              green: greenButton,
@@ -109,14 +105,10 @@ function playLastSequence() {
             lightsOff()
         }, (i + 1) * 1000)
         }
-        // function test () {colors[game.lightSequence[i]].style.opacity = '0.5'}
         lightUp()
     }
 }
 
-function scoreBoard () {
-    var score = document.getElementsByClassName('score')[0].textContent = `Score: ${game.round}`
+const scoreBoard = () => {
+    let score = document.getElementsByClassName('score')[0].textContent = `Score: ${game.round}`
 }
-
-// opacity https://www.w3schools.com/jsref/prop_style_opacity.asp
-// random numbers javascript & jquery book
